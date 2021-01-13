8b34ae72c4c324e477a1bee4e6b561c0719c3d2f
bpotts11 <brittneympotts@gmail.com>
2021-01-13 10:04:04 -0600

    created a javascript file to contain info about fish. ch8

---
+++ scripts/FishDataProvider.js
@@ -0,0 +1,51 @@
+const fishCollection = [
+    {
+
+        name: "Marlin",
+        species: "Amphiprionanae",
+        location: "Great Barrier Reef",
+        length: "4 inches",
+        diet: "seafood",
+        image: "",
+    }
+]
+const fishCollection = [
+    {
+        name: "Nemo",
+        species: "Amphiprionanae",
+        location: "Great Barrier Reef",
+        length: "2 inches",
+        diet: "seafood",
+        image: "",
+    }
+]
+const fishCollection = [
+    {
+        name: "Dory",
+        species: "Blue Tang",
+        location: "Great Barrier Reef",
+        length: "4 inches",
+        diet: "algae",
+        image: "",
+    }
+]
+const fishCollection = [
+    {
+        name: "Gill",
+        species: "Moorish Idol",
+        location: "Pacific Ocean",
+        length: "6 inches",
+        diet: "seafood",
+        image: "",
+    }
+]
+const fishCollection = [
+    {
+        name: "Bubbles",
+        species: "Yellow Tang",
+        location: "Pacific Ocean",
+        length: "5 inches",
+        diet: "herbivore",
+        image: "",
+    }
+]
