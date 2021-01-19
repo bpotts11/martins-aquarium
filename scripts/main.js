18c9ff1303df342f493f70f1f836c3990e5afcfe
bpotts11 <brittneympotts@gmail.com>
2021-01-19 10:14:06 -0600

    sorted fish into holy,soldier, and regular fish

--- scripts/main.js
+++ scripts/main.js
@@ -1,34 +1,8 @@
-import { useFish } from './fish/FishDataProvider.js'
 import { FishList } from './fish/FishList.js'
-
-const allTheFish = useFish()
-
-for (const fish of allTheFish) {
-    console.log(fish)
-}
-
 FishList()
 
-
-import { useQuote } from './tips/TipDataProvider.js'
 import { TipList } from './tips/TipList.js'
-
-const allTheQuote = useQuote()
-
-for (const quote of allTheQuote) {
-    console.log(quote)
-}
-
 TipList()
 
-
-import { useLocation } from './locations/LocationDataProvider.js'
 import { LocationList } from './locations/LocationList.js'
-
-const allTheLocation = useLocation()
-
-for (const location of allTheLocation) {
-    console.log(location)
-}
-
 LocationList()
